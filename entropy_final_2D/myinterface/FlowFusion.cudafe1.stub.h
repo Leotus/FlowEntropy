fd152a55724720543df413bc2b3bc5c98c212fd9
leeten <leeten@eaa6695f-5702-45ae-85b7-ecd018c7cb11>
2009-12-07 20:18:56 +0000

    [12/07/2009] 1. [KIL] Remove the CUDA temp. files.
    git-svn-id: svn+ssh://repo.recheliu.org/home/leeten/ext/repos/SVN/FlowEntropy/trunk@9 eaa6695f-5702-45ae-85b7-ecd018c7cb11

--- entropy_final_2D/myinterface/FlowFusion.cudafe1.stub.h
+++
@@ -1,2 +0,0 @@
-static void __device_stub__Z20_FlowFusion2D_kernelfii14cudaPitchedPtrS_(float, int, int, struct cudaPitchedPtr __cppref__ , struct cudaPitchedPtr __cppref__ );
-static void __device_stub__Z18_FlowFusion_kernelfiiiiii14cudaPitchedPtrS_(float, int, int, int, int, int, int, struct cudaPitchedPtr __cppref__ , struct cudaPitchedPtr __cppref__ );
