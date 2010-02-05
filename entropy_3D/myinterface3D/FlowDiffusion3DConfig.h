f3706e96940a3c5f3951faadc097782191ba7021
leeten <leeten@eaa6695f-5702-45ae-85b7-ecd018c7cb11>
2010-02-05 01:29:30 +0000

    [02/02/2010] 1. [ADD] Add a new preprocessor DIFFUSION_ON_GLSL and include a header FlowDiffusionGLSLLib.h 2. [MOD] Change the preprocessors BIN_LOOKUP_ON_GPU, DIFFUSION_ON_GPU, ENTROPY_ON_GPU to BIN_LOOKUP_ON_CUDA, DIFFUSION_ON_CUDA, ENTROPY_ON_CUDA, respectively. 3. [ADD] Add a new preprocessor PRUNING_THRESHOLD. 4. [ADD] Add a new preprocessor NR_OF_SAMPLES.
    git-svn-id: svn+ssh://repo.recheliu.org/home/leeten/ext/repos/SVN/FlowEntropy/trunk@142 eaa6695f-5702-45ae-85b7-ecd018c7cb11

--- entropy_3D/myinterface3D/FlowDiffusion3DConfig.h
+++ entropy_3D/myinterface3D/FlowDiffusion3DConfig.h
@@ -1,20 +1,43 @@
 #ifndef __FLOW_DIFFUSION_CUDA_H__
 #define	__FLOW_DIFFUSION_CUDA_H__
 
+// ADD-BY-LEETEN 02/04/2010-BEGIN
+#define	DIFFUSION_ON_GLSL	0
+#include "FlowDiffusionGLSLLib/FlowDiffusionGLSLLib.h"
+// ADD-BY-LEETEN 02/04/2010-END
+
+#define CLOCK_reconstruct_field_GVF_3D	0
+
 #define USE_CUDA			1
-#define ENTER_GLUT_LOOP		0
+#define ENTER_GLUT_LOOP		1
 #define BIN_LOOKUP			1
+
 
 // ADD-BY-LEETEN 02/02/2010-BEGIN
 #if	USE_CUDA
-	#define DIFFUSION_ON_GPU	1
-	#define BIN_LOOKUP_ON_GPU	1
+	#define DIFFUSION_ON_CUDA	1
+	// MOD-BY-LEETEN 02/04/2010-FROM:
+		// #define BIN_LOOKUP_ON_GPU	1
+	// TO:
+	#define BIN_LOOKUP_ON_CUDA	1
+	// MOD-BY-LEETEN 02/04/2010-END
 
-	#if		BIN_LOOKUP_ON_GPU
-		#define	ENTROPY_ON_GPU		0
-	#endif	// #if	BIN_LOOKUP_ON_GPU	
+	#if		BIN_LOOKUP_ON_CUDA
+		// MOD-BY-LEETEN 02/04/2010-FROM:
+			// #define	ENTROPY_ON_GPU		0
+		// TO:	
+		#define	ENTROPY_ON_CUDA		0
+		// MOD-BY-LEETEN 02/04/2010-END
+	#endif	// #if	BIN_LOOKUP_ON_CUDA	
 #endif	// #if	USE_CUDA
 // ADD-BY-LEETEN 02/02/2010-END
+
+// ADD-BY-LEETEN 02/04/2010-BEGIN
+#if	DIFFUSION_ON_CUDA
+	#define	DIFFUSION_ON_GLSL	0
+#endif
+
+// ADD-BY-LEETEN 02/04/2010-END
 
 // ADD-BY-LEETEN 02/02/2010-BEGIN
 #define KERNEL_HALF_WIDTH	8	
@@ -23,9 +46,14 @@
 // ADD-BY-LEETEN 02/02/2010-END
 
 // ADD-BY-LEETEN 12/14/2009-BEGIN
-// DEL-BY-LEETEN 02/02/2010		#define BIN_LOOKUP_ON_GPU	1
+// DEL-BY-LEETEN 02/02/2010		#define BIN_LOOKUP_ON_CUDA	1
 #define KEEP_BOUNDARY		0
 // ADD-BY-LEETEN 12/14/2009-END
+
+// ADD-BY-LEETEN 02/04/2010-BEGIN
+#define PRUNING_THRESHOLD	0.001
+#define	NR_OF_SAMPLES		((grid_res[0] * grid_res[1] * grid_res[2]) / 512)
+// ADD-BY-LEETEN 02/04/2010-END
 
 #define NR_OF_STREAMLINES	1
 
@@ -69,6 +97,11 @@
 /*
 
 $Log: not supported by cvs2svn $
+Revision 1.1  2010/02/03 00:40:28  leeten
+
+[02/02/2010]
+1. [1ST] First time checkin.
+
 Revision 1.4  2010/01/06 17:28:36  leeten
 
 [01/06/2010]
@@ -85,7 +118,7 @@ Revision 1.3  2010/01/04 18:02:38  leeten
 Revision 1.2  2009/12/15 20:08:17  leeten
 
 [12/15/2009]
-1. [ADD] Define a new preprocessor BIN_LOOKUP_ON_GPU. If it is 1, the vector field is converted to the discrete bin via CUDA.
+1. [ADD] Define a new preprocessor BIN_LOOKUP_ON_CUDA. If it is 1, the vector field is converted to the discrete bin via CUDA.
 2. [ADD] Define a new preprocessor KEEP_BOUNDARY. If it is 1, the vector field along the boundary will be preserved.
 
 Revision 1.1  2009/12/07 20:16:04  leeten
