3852b4353c7b5d94aa16ec87a84909321283b465
leeten <leeten@eaa6695f-5702-45ae-85b7-ecd018c7cb11>
2010-03-10 20:32:40 +0000

    [03/10/2010] 1. [MOD] When compute the histogram, bin 0 is reserved for vector with zero length. 2. [ADD] Compute the RMSE and error rate too. 3. [ADD] Add a new switch '--max-nr-of-streamlines' to specify the variable iMaxNrOfStreamlines, which is used to specify the max. #streamlines to be tested.
    git-svn-id: svn+ssh://repo.recheliu.org/home/leeten/ext/repos/SVN/FlowEntropy/trunk@170 eaa6695f-5702-45ae-85b7-ecd018c7cb11

--- StreamlineEntropy/StreamlineEntropy_main.cpp
+++ StreamlineEntropy/StreamlineEntropy_main.cpp
@@ -26,8 +26,19 @@
 	const int iNrOfBins = 360;
 	TBuffer<float2>		pf2Thetas;
 	TBuffer<float2>		pf2Phis;
-	int ppiJointHistogram[iNrOfBins][iNrOfBins];
-	int piMarginalHistogram[iNrOfBins];
+	#if	0	// MOD-BY-LEETEN 03/10/2010-FROM:
+		int ppiJointHistogram[iNrOfBins][iNrOfBins];
+		// MOD-BY-LEETEN 02/19/2010-FROM:
+			// int piMarginalHistogram[iNrOfBins];
+		// TO:
+		int piInputMarginalHistogram[iNrOfBins];
+		int piOutputMarginalHistogram[iNrOfBins];
+		// MOD-BY-LEETEN 02/19/2010-END
+	#else	// MOD-BY-LEETEN 03/10/2010-TO:
+	int ppiJointHistogram[iNrOfBins+1][iNrOfBins+1];
+	int piInputMarginalHistogram[iNrOfBins+1];
+	int piOutputMarginalHistogram[iNrOfBins+1];
+	#endif	// MOD-BY-LEETEN 03/10/2010-END
 
 	const int iNrOfThetas = 720;
 	const int iNrOfPhis = 360;
@@ -165,14 +176,22 @@ _BuildLookupTable()
 			double dTheta =	M_PI * 2.0 * double(t) / double(iNrOfThetas);
 			double dPhi =	M_PI * double(p) / double(iNrOfPhis);
 			int iBin = IGetBinByAngle(float(dTheta), float(dPhi), iNrOfBins, &pf2Thetas[0], &pf2Phis[0]);
-			if( iBin >= 0 )
-				ppiAngleMap[t][p] = iBin;
+			// MOD-BY-LEETEN 03/10/2010-FROM:
+				// if( iBin >= 0 )
+			// TO:
+			assert(iBin>=0);
+			// MOD-BY-LEETEN 03/10/2010-END
+
+				// MOD-BY-LEETEN 03/10/2010-FROM:
+					// ppiAngleMap[t][p] = iBin;
+				// TO:
+				ppiAngleMap[t][p] = iBin + 1;	// bin zero is preserved
+				// MOD-BY-LEETEN 03/10/2010-END
 		}
 }
 
 double DGetAngle(double x, double y)
 {
-
 	if((0.0 == x)&&(0.0 == y))
 		return 0.0;
 	else
@@ -183,7 +202,6 @@ double DGetAngle(double x, double y)
 
 double DGetAngle2(double x, double y)
 {
-
 	if((0.0 == x)&&(0.0 == y))
 		return 0.0;
 	else
@@ -238,6 +256,16 @@ main(int argn, char* argv[])
 		"--streamline-filename", 
 		"Specify the file that contains the streamlines.");
 
+	// ADD-BY-LEETEN 03/10/2010-BEGIN
+	int iMaxNrOfStreamlines;
+	_OPTAddIntegerVector(
+		"--max-nr-of-streamlines", 1,
+		&iMaxNrOfStreamlines, 500);
+	_OPTAddComment(
+		"--max-nr-of-streamlines", 
+		"Specify the max. #streamlines to be tested.");
+	// ADD-BY-LEETEN 03/10/2010-END
+
 	assert( BOPTParse(argv, argn, 1) );
 
 	assert(szStreamlineFilename);
@@ -274,16 +302,32 @@ main(int argn, char* argv[])
 			iZ = max(min(iZ, p3Df3VectorField.iDepth - 1), 0);
 			int iVoxel = iX + iY * p3Df3VectorField.iWidth + iZ * p3Df3VectorField.iWidth * p3Df3VectorField.iHeight;
 
-			float fLength = 0.0;
-			for(int i = 0; i < 3; i++)
-				fLength += powf(p3Df3VectorField[iVoxel][i], 2.0);
-
-			if(fLength  > 0.0)
-			{
-				fLength  = sqrt(fLength);
+			#if	0	// MOD-BY-LEETEN 03/10/2010-FROM:
+				float fLength = 0.0;
 				for(int i = 0; i < 3; i++)
-					p3Df3InitVectorField[iVoxel][i] = p3Df3VectorField[iVoxel][i] / fLength;
+					fLength += powf(p3Df3VectorField[iVoxel][i], 2.0);
+
+				if(fLength  > 0.0)
+				{
+					fLength  = sqrt(fLength);
+					for(int i = 0; i < 3; i++)
+						p3Df3InitVectorField[iVoxel][i] = p3Df3VectorField[iVoxel][i] / fLength;
+				}
+			#else	// MOD-BY-LEETEN 03/10/2010-TO:
+			double dLength = 0.0;
+			for(int i = 0; i < 3; i++)
+			{
+				double d = double(p3Df3VectorField[iVoxel][i]);
+				dLength += d * d;
 			}
+
+			if(dLength  > 0.0)
+			{
+				dLength  = sqrt(dLength);
+				for(int i = 0; i < 3; i++)
+					p3Df3InitVectorField[iVoxel][i] = p3Df3VectorField[iVoxel][i] / float(dLength);
+			}
+			#endif	// MOD-BY-LEETEN 03/10/2010-END
 
 			// compute the coefficients 
 			p3Df4OffsetWeight[iVoxel].w = 
@@ -338,7 +382,11 @@ main(int argn, char* argv[])
 
 
 			dDiffusionError = sqrt(dDiffusionError / double(3 * p3Df3VectorField.USize()));
-			// LOG_VAR(dDiffusionError);
+
+			// LOG_VAR(dDiffusionError);	
+			// ADD-BY-LEETEN 03/10/2010-BEGIN
+			fprintf(stderr, "%e\r", dDiffusionError);
+			// ADD-BY-LEETEN 03/10/2010-END
 			static double dPrevDiffusionError = 0.0;
 
 			if( iIter  > max(max(p3Df3VectorField.iWidth, p3Df3VectorField.iHeight), p3Df3VectorField.iDepth) )
@@ -349,7 +397,15 @@ main(int argn, char* argv[])
 					// LOG_VAR(dDiffusionError);
 					break;
 				}
+
 			}
+			// ADD-BY-LEETEN 03/10/2010-BEGIN
+			if( iIter > 0 && dPrevDiffusionError < dDiffusionError)
+			{
+				fprintf(stderr, "Error increases. Stop. \n");
+				break;
+			}
+			// ADD-BY-LEETEN 03/10/2010-END
 
 			dPrevDiffusionError  = dDiffusionError;
 			memcpy(&p3Df3TempVectorField[0], &p3Df3ResultVectorField[0], sizeof(p3Df3TempVectorField[0]) * p3Df3TempVectorField.USize());
@@ -358,11 +414,55 @@ main(int argn, char* argv[])
 
 		// construct the histogram
 		memset(ppiJointHistogram, 0,	sizeof(ppiJointHistogram));
-		memset(piMarginalHistogram, 0,	sizeof(piMarginalHistogram));
+		// MOD-BY-LEETEN 02/19/2010-FROM:
+			// memset(piMarginalHistogram, 0,	sizeof(piMarginalHistogram));
+		// TO:
+		memset(piInputMarginalHistogram,	0,	sizeof(piInputMarginalHistogram));
+		memset(piOutputMarginalHistogram,	0,	sizeof(piOutputMarginalHistogram));
+		// MOD-BY-LEETEN 02/19/2010-END
+
+		// ADD-BY-LEETEN 03/10/2010-BEGIN
+		double dRMSE = 0.0;
+		double dErrorRate = 0.0;
+		// ADD-BY-LEETEN 03/10/2010-END
+
 		for(int	iVoxel = 0, iZ = 0; iZ < p3Df3VectorField.iDepth;	iZ++)
 			for(int			iY = 0; iY < p3Df3VectorField.iHeight;	iY++)
 				for(int		iX = 0; iX < p3Df3VectorField.iWidth;	iX++, iVoxel++)
 				{
+
+					// ADD-BY-LEETEN 03/10/2010-BEGIN
+					double dLength;
+					
+					dLength = 
+						pow(double(p3Df3VectorField[iVoxel][0]), 2.0) +  
+						pow(double(p3Df3VectorField[iVoxel][1]), 2.0) +  
+						pow(double(p3Df3VectorField[iVoxel][2]), 2.0);
+					if( dLength > 0.0)
+					{
+						dLength = sqrt(dLength);
+						for(int j = 0; j < 3; j++)
+							p3Df3VectorField[iVoxel][j] = float(double(p3Df3VectorField[iVoxel][j])/dLength);
+					}
+
+					dLength = 
+						pow(double(p3Df3ResultVectorField[iVoxel][0]), 2.0) +  
+						pow(double(p3Df3ResultVectorField[iVoxel][1]), 2.0) +  
+						pow(double(p3Df3ResultVectorField[iVoxel][2]), 2.0);
+					if( dLength > 0.0)
+					{
+						dLength = sqrt(dLength);
+						for(int j = 0; j < 3; j++)
+							p3Df3ResultVectorField[iVoxel][j] = float(double(p3Df3ResultVectorField[iVoxel][j])/dLength);
+					}
+
+					for(int j = 0; j < 3; j++)
+					{
+						double dDiff  = double(p3Df3ResultVectorField[iVoxel][j] - p3Df3VectorField[iVoxel][j]);
+						dRMSE += dDiff * dDiff;
+					}
+					// ADD-BY-LEETEN 03/10/2010-END
+
 					int iBin = IGetBin(
 						p3Df3VectorField[iVoxel][0], 
 						p3Df3VectorField[iVoxel][1], 
@@ -373,21 +473,59 @@ main(int argn, char* argv[])
 						p3Df3ResultVectorField[iVoxel][2]);
 
 					ppiJointHistogram[iNewBin][iBin]++;
-					piMarginalHistogram[iNewBin]++;
+					// MOD-BY-LEETEN 02/19/2010-FROM:
+						// piMarginalHistogram[iNewBin]++;
+					// TO:
+					piInputMarginalHistogram[iBin]++;
+					piOutputMarginalHistogram[iNewBin]++;
+					// MOD-BY-LEETEN 02/19/2010-END
+
+					// ADD-BY-LEETEN 03/10/2010-BEGIN
+					if( iNewBin != iBin )
+						dErrorRate += 1.0;
+					// ADD-BY-LEETEN 03/10/2010-END
 				}
 
 		double dJointEntropy = 0.0;
-		double dMarginalEntropy = 0.0;
+		// MOD-BY-LEETEN 02/19/2010-FROM:
+			// double dMarginalEntropy = 0.0;
+		// TO:
+		double dOutputMarginalEntropy = 0.0;
+		double dInputMarginalEntropy = 0.0;
+		// MOD-BY-LEETEN 02/19/2010-END
+
 		double dCondEntropy;
 		double dNrOfVoxels = double(p3Df3VectorField.USize());
 
 		for(int h = 0; h < iNrOfBins; h++)
 		{
-			if( 0 == piMarginalHistogram[h] )
-				continue;
+			#if	0	// MOD-BY-LEETEN 02/19/2010-FROM:
+				if( 0 == piMarginalHistogram[h] )
+					continue;
 
-			double dProb = double(piMarginalHistogram[h])/dNrOfVoxels;
-			dMarginalEntropy += -dProb * log(dProb)/log(2.0);
+				double dProb = double(piMarginalHistogram[h])/dNrOfVoxels;
+				dMarginalEntropy += -dProb * log(dProb)/log(2.0);
+			#else	// MOD-BY-LEETEN 02/19/2010-TO:
+			if( piInputMarginalHistogram[h] > 0 )
+			{
+				double dProb = double(piInputMarginalHistogram[h])/dNrOfVoxels;
+				dInputMarginalEntropy += -dProb * log(dProb)/log(2.0);
+			}
+
+			if( piOutputMarginalHistogram[h] > 0 )
+			{
+				double dProb = double(piOutputMarginalHistogram[h])/dNrOfVoxels;
+				dOutputMarginalEntropy += -dProb * log(dProb)/log(2.0);
+			}
+
+			#if	0	// DEL-BY-LEETEN 03/10/2010-BEGIN
+				if( 0 == piOutputMarginalHistogram[h] )
+					continue;
+
+				double dProb;
+			#endif	// DEL-BY-LEETEN 03/10/2010-END
+
+			#endif	// MOD-BY-LEETEN 02/19/2010-END
 
 			for(int w = 0; w < iNrOfBins; w++)
 			{
@@ -398,8 +536,51 @@ main(int argn, char* argv[])
 				dJointEntropy += -dProb * log(dProb)/log(2.0);
 			}
 		}
-		dCondEntropy = dJointEntropy - dMarginalEntropy;
+
+		// ADD-BY-LEETEN 03/10/2010-BEGIN
+		LOG_VAR(uS);
+		// ADD-BY-LEETEN 03/10/2010-END
+
+		// ADD-BY-LEETEN 03/10/2010-BEGIN
+		dRMSE /= double(3 * p3Df3VectorField.USize());
+		dRMSE = sqrt(dRMSE);
+		LOG_VAR(dRMSE);
+
+		dErrorRate /= double(p3Df3VectorField.USize());
+		LOG_VAR(dErrorRate);
+		// ADD-BY-LEETEN 03/10/2010-END
+
+		// MOD-BY-LEETEN 02/19/2010-FROM:
+			// dCondEntropy = dJointEntropy - dMarginalEntropy;
+		// TO:
+		dCondEntropy = dJointEntropy - dOutputMarginalEntropy;
+		// MOD-BY-LEETEN 02/19/2010-END
 		LOG_VAR(dCondEntropy);
+
+		// ADD-BY-LEETEN 02/19/2010-BEGIN
+		#if	0	// MOD-BY-LEETEN 03/10/2010-FROM:
+		static double dPrevCondEntropy;
+
+		if( uS > 0 )
+		{
+			double dCondEntropyRatio = dCondEntropy / dPrevCondEntropy;
+			if( 0.9999 < dCondEntropyRatio && dCondEntropyRatio <= 1.0)
+				break;
+		}
+
+		dPrevCondEntropy = dCondEntropy;
+		#else	// MOD-BY-LEETEN 03/10/2010-TO:
+		if( int(uS) > iMaxNrOfStreamlines )
+			break;
+		#endif	// MOD-BY-LEETEN 03/10/2010-END
+		// ADD-BY-LEETEN 02/19/2010-END
+
+		#if	0	// DEL-BY-LEETEN 03/10/2010-BEGIN
+			// ADD-BY-LEETEN 02/19/2010-BEGIN
+			double dMutualInformation = dInputMarginalEntropy + dOutputMarginalEntropy - dJointEntropy;
+			LOG_VAR(dMutualInformation);
+			// ADD-BY-LEETEN 02/19/2010-END
+		#endif	// DEL-BY-LEETEN 03/10/2010-END
 	}
 
 	// compute the cond. entropy
@@ -410,5 +591,10 @@ main(int argn, char* argv[])
 /*
 
 $Log: not supported by cvs2svn $
+Revision 1.1  2010/02/16 19:51:27  leeten
+
+[02/16/2010]
+1. [1ST] First time checkin.
+
 
 */
